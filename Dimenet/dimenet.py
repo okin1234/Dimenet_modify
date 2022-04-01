bb94203647fb43b267adfef8d5a388dbc471813b
Junhyeong Gu <rnwnsgud1234@postech.ac.kr>
2022-04-01 22:38:12 +0900

    first commit
    layers의 interaction, output block ++ version으로 수정 필요
    main dimnet model 파일은 ++버전으로 추가 수정 필요 (dimenet github에서 original이랑 ++버전 차이 봐서 업데이트 하셈)

---
+++ Dimenet/dimenet.py
@@ -0,0 +1,159 @@
+import os
+import os.path as osp
+from math import pi as PI
+from math import sqrt
+from typing import Callable
+
+import numpy as np
+import torch
+from torch.nn import Embedding, Linear
+from torch_scatter import scatter
+from torch_sparse import SparseTensor
+
+from torch_geometric.data import Dataset, download_url
+from torch_geometric.data.makedirs import makedirs
+from torch_geometric.nn import radius_graph
+
+from torch_geometric.nn.inits import glorot_orthogonal
+from torch_geometric.nn.acts import swish
+
+from .layers import *
+
+
+
+class DimeNet(torch.nn.Module):
+    """
+    Args:
+        hidden_channels (int): Hidden embedding size.
+        out_channels (int): Size of each output sample.
+        num_blocks (int): Number of building blocks.
+        num_bilinear (int): Size of the bilinear layer tensor.
+        num_spherical (int): Number of spherical harmonics.
+        num_radial (int): Number of radial basis functions.
+        cutoff: (float, optional): Cutoff distance for interatomic
+            interactions. (default: :obj:`5.0`)
+        max_num_neighbors (int, optional): The maximum number of neighbors to
+            collect for each node within the :attr:`cutoff` distance.
+            (default: :obj:`32`)
+        envelope_exponent (int, optional): Shape of the smooth cutoff.
+            (default: :obj:`5`)
+        num_before_skip: (int, optional): Number of residual layers in the
+            interaction blocks before the skip connection. (default: :obj:`1`)
+        num_after_skip: (int, optional): Number of residual layers in the
+            interaction blocks after the skip connection. (default: :obj:`2`)
+        num_output_layers: (int, optional): Number of linear layers for the
+            output blocks. (default: :obj:`3`)
+        act: (Callable, optional): The activation funtion.
+            (default: :obj:`swish`)
+    """
+
+    def __init__(self, hidden_channels: int, out_channels: int,
+                 num_blocks: int, num_bilinear: int, num_spherical: int,
+                 num_radial, cutoff: float = 5.0, max_num_neighbors: int = 32,
+                 envelope_exponent: int = 5, num_before_skip: int = 1,
+                 num_after_skip: int = 2, num_output_layers: int = 3,
+                 act: Callable = swish):
+        super().__init__()
+
+        self.cutoff = cutoff
+        self.max_num_neighbors = max_num_neighbors
+        self.num_blocks = num_blocks
+
+        self.rbf = BesselBasisLayer(num_radial, cutoff, envelope_exponent)
+        self.sbf = SphericalBasisLayer(num_spherical, num_radial, cutoff,
+                                       envelope_exponent)
+
+        self.emb = EmbeddingBlock(num_radial, hidden_channels, act)
+
+        self.output_blocks = torch.nn.ModuleList([
+            OutputBlock(num_radial, hidden_channels, out_channels,
+                        num_output_layers, act) for _ in range(num_blocks + 1)
+        ])
+
+        self.interaction_blocks = torch.nn.ModuleList([
+            InteractionBlock(hidden_channels, num_bilinear, num_spherical,
+                             num_radial, num_before_skip, num_after_skip, act)
+            for _ in range(num_blocks)
+        ])
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.rbf.reset_parameters()
+        self.emb.reset_parameters()
+        for out in self.output_blocks:
+            out.reset_parameters()
+        for interaction in self.interaction_blocks:
+            interaction.reset_parameters()
+
+    def make_training_data():
+        model = DimeNet(hidden_channels=128, out_channels=1, num_blocks=6,
+                        num_bilinear=8, num_spherical=7, num_radial=6,
+                        cutoff=5.0, envelope_exponent=5, num_before_skip=1,
+                        num_after_skip=2, num_output_layers=3)
+
+
+        # Use the same random seed as the official DimeNet` implementation.
+        random_state = np.random.RandomState(seed=42)
+        perm = torch.from_numpy(random_state.permutation(np.arange(130831)))
+        train_idx = perm[:110000]
+        val_idx = perm[110000:120000]
+        test_idx = perm[120000:]
+
+        return model, (dataset[train_idx], dataset[val_idx], dataset[test_idx])
+
+    def triplets(self, edge_index, num_nodes):
+        row, col = edge_index  # j->i
+
+        value = torch.arange(row.size(0), device=row.device)
+        adj_t = SparseTensor(row=col, col=row, value=value,
+                             sparse_sizes=(num_nodes, num_nodes))
+        adj_t_row = adj_t[row]
+        num_triplets = adj_t_row.set_value(None).sum(dim=1).to(torch.long)
+
+        # Node indices (k->j->i) for triplets.
+        idx_i = col.repeat_interleave(num_triplets)
+        idx_j = row.repeat_interleave(num_triplets)
+        idx_k = adj_t_row.storage.col()
+        mask = idx_i != idx_k  # Remove i == k triplets.
+        idx_i, idx_j, idx_k = idx_i[mask], idx_j[mask], idx_k[mask]
+
+        # Edge indices (k-j, j->i) for triplets.
+        idx_kj = adj_t_row.storage.value()[mask]
+        idx_ji = adj_t_row.storage.row()[mask]
+
+        return col, row, idx_i, idx_j, idx_k, idx_kj, idx_ji
+
+    def forward(self, z, pos, batch=None):
+        """"""
+        edge_index = radius_graph(pos, r=self.cutoff, batch=batch,
+                                  max_num_neighbors=self.max_num_neighbors)
+
+        i, j, idx_i, idx_j, idx_k, idx_kj, idx_ji = self.triplets(
+            edge_index, num_nodes=z.size(0))
+
+        # Calculate distances.
+        dist = (pos[i] - pos[j]).pow(2).sum(dim=-1).sqrt()
+
+        # Calculate angles.
+        pos_i = pos[idx_i]
+        pos_ji, pos_ki = pos[idx_j] - pos_i, pos[idx_k] - pos_i
+        a = (pos_ji * pos_ki).sum(dim=-1)
+        b = torch.cross(pos_ji, pos_ki).norm(dim=-1)
+        angle = torch.atan2(b, a)
+
+        rbf = self.rbf(dist)
+        sbf = self.sbf(dist, angle, idx_kj)
+
+        # Embedding block.
+        x = self.emb(z, rbf, i, j)
+        P = self.output_blocks[0](x, rbf, i, num_nodes=pos.size(0))
+
+        # Interaction blocks.
+        for interaction_block, output_block in zip(self.interaction_blocks,
+                                                   self.output_blocks[1:]):
+            x = interaction_block(x, rbf, sbf, idx_kj, idx_ji)
+            P += output_block(x, rbf, i)
+
+        return P.sum(dim=0) if batch is None else scatter(P, batch, dim=0)
+
